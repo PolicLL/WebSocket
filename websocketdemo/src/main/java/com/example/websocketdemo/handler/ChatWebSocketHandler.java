65c451347cb5ad3831edd87f7b959dd1f1a4518f
Luka <LPolic@its.jnj.com>
2025-03-20 15:14:08 +0100

    Removed endpoint.

--- websocketdemo/src/main/java/com/example/websocketdemo/handler/ChatWebSocketHandler.java
+++ websocketdemo/src/main/java/com/example/websocketdemo/handler/ChatWebSocketHandler.java
@@ -1,31 +1,29 @@
 package com.example.websocketdemo.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 @Component
+@Slf4j
 public class ChatWebSocketHandler extends TextWebSocketHandler {
 
   @Override
   public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-    // Handle connection establishment (e.g., add to a list of connected users)
-    System.out.println("New WebSocket connection established: " + session.getId());
+    log.info("New WebSocket connection established: " + session.getId());
   }
 
   @Override
   protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-    // Handle incoming text messages
     String payload = message.getPayload();
-    System.out.println("Message received: " + payload);
-    // Optionally send a response back to the client
+    log.info("Message received: " + payload);
     session.sendMessage(new TextMessage("Received your message: " + payload));
   }
 
   @Override
   public void afterConnectionClosed(WebSocketSession session, org.springframework.web.socket.CloseStatus status) throws Exception {
-    // Handle connection closure
-    System.out.println("WebSocket connection closed: " + session.getId());
+    log.info("WebSocket connection closed: " + session.getId());
   }
 }
